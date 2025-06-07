c81a80554e862278bc234906a5ed21e55f1aeccf
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-07 16:07:23 +0530

    Updation

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -6,7 +6,7 @@ export const register=async(req,res)=>{
         const {name,email,password}=req.body;
     const token=await registerUser(name,email,password);
     res.cookie('token',token,cookieOptions)
-    res.status(200).json({message:"User registered successfully"});
+    res.status(200).json({message:"User registered successfully", user:{name,email}});
     }catch(err){
         console.error(err);
         res.status(500).json({message:"Internal Server Error", error: err.message});
@@ -18,7 +18,7 @@ export const login=async(req,res)=>{
         const {email,password}=req.body;
     const{ token,user}=await loginUser(email,password);
     res.cookie('token',token,cookieOptions);
-    res.status(200).json({message:"User logged in successfully"})
+    res.status(200).json({message:"User logged in successfully", user});
     }
     catch(err){
         console.error(err);
