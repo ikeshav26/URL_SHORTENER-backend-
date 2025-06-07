47d8bb9308783e37622a0c82d216ddf761df23cc
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-07 21:16:28 +0530

    Locally fixed commit

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,35 +3,38 @@ import { createUser, findUserByEmail, findUserById } from '../dao/user.dao.js';
 import { signToken } from '../utils/helper.js';
 import bcrypt from 'bcrypt';
 
-export const registerUser=async(name,email,password)=>{
-    try{
-        const user=await findUserByEmail(email);
-    if(user){
-        throw new Error('User already exists');
-    }
+export const registerUser = async (name, email, password) => {
+    try {
+        const user = await findUserByEmail(email);
+        if (user) {
+            throw new Error('User already exists');
+        }
 
-    const newUser=await createUser(name,email,password);
-    const token=await signToken({id:newUser._id})
-    return token;
+        const newUser = await createUser(name, email, password);
+        const token = await signToken({ id: newUser._id })
+        return token;
     }
-    catch(err){
+    catch (err) {
         console.error(err);
         throw new Error('Error registering user', err.message);
     }
 }
 
 
-export const loginUser=async(email,password)=>{
-    try{
-        const user=await findUserByEmail(email);
-    const isPasswordValid=bcrypt.compareSync(password, user.password);
-    if(!user || !isPasswordValid){
-        throw new Error('Invalid Credentials');
+export const loginUser = async (email, password) => {
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            throw new Error('Invalid Credentials');
+        }
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+        if (!isPasswordValid) {
+            throw new Error('Invalid Credentials');
+        }
+        const token = await signToken({ id: user._id })
+        return { token, user };
     }
-    const token=await signToken({id:user._id})
-    return {token,user};
-    }
-    catch(err){
+    catch (err) {
         console.error(err);
         throw new Error('Error logging in user', err.message);
     }
