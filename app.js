2edc6c5d92047f3603f87f8de70cf4014636f907
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-08 23:39:36 +0530

    Url update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:'https://url-shortener-frontend-gray.vercel.app', // your frontend URL
+    origin:'https://urlshortener.ikeshav.tech/', // your frontend URL
   credentials: true
 }));
 
