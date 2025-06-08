e18745dd5402f66720940768f6a66928ab99289f
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-08 15:34:43 +0530

    Build fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions={
     httpOnly:true,
-    secure:false, // Set to true in production
-    sameSite:'lax', 
-    maxAge:60*60*24*30 // 5 minutes
+    secure:false, 
+    sameSite:'Strict', 
+    maxAge:60*60*24*30 
 }
