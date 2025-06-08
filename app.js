75be2bbc38bf8a0c267d3018005bc61dc655a259
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-08 23:59:34 +0530

    fix commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:'https://urlshortener.ikeshav.tech/', // your frontend URL
+    origin:'https://urlshortener.ikeshav.tech', // your frontend URL
   credentials: true
 }));
 
